0df1c07cde9bebaa2a03d0552416d0683e4a4207
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-20 23:00:50 -0300

    Fix auto-fixable problems in index.tsx and App.tsx

--- mypokelist/src/App.tsx
+++ mypokelist/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
       Hello World
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
