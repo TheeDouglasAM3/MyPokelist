83f649d8e6a52cf6960a1fcc5c857a53704c4b95
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-23 22:24:54 -0300

    Update Routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
+import PokemonList from './pages/PokemonList'
 
 function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" component={Home} exact />
+      <Route path="/pokemon-list" component={PokemonList} exact />
     </BrowserRouter>
   )
 }
