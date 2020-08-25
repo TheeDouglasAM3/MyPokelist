5d92d7938174e80abef315fe2fb98431a54a1dc0
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-24 23:21:34 -0300

    Update page and styles PokemonList

--- src/pages/PokemonList/index.tsx
+++ src/pages/PokemonList/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import api from '../../services/api'
+
+import PokemonDisplay from '../../components/PokemonDiplay'
 
 import './styles.css'
 
+interface PokemonDisplayProps {
+  name: string
+  number: number
+  image: string
+  poketypes: string
+}
 function PokemonList() {
+  const [pokemons, setPokemons] = useState([])
+
+  async function searchDetailsSinglePokemon(url: string) {
+    const response = await api.get(url)
+    return response.data
+  }
+
+  async function searchPokemons() {
+    const response = await api.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=40')
+  }
+
+  useEffect(() => {
+    searchPokemons()
+  }, [])
+
   return (
     <div id="page-pokemon-list">
       <div className="navbar">
@@ -18,9 +42,16 @@ function PokemonList() {
       </div>
 
       <div id="pokemon-list-area">
-        <div className="pokemon-area">
-          pokemon
-        </div>
+        {pokemons.map((pokemon: PokemonDisplayProps) => (
+          <PokemonDisplay
+            key={pokemon.number}
+            name={pokemon.name}
+            number={pokemon.number}
+            image={pokemon.image}
+            poketypes={pokemon.poketypes}
+          />
+        ))}
+
       </div>
 
     </div>
