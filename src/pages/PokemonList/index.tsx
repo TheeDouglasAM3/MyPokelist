e2a1c8e7c464cf9b9cbfd3b1ab5f5a8b7df7008b
Douglas Marcelino <douglas_champion@hotmail.com>
2020-09-03 18:56:25 -0300

    Add method search pokemon in PokemonList page

--- src/pages/PokemonList/index.tsx
+++ src/pages/PokemonList/index.tsx
@@ -12,16 +12,10 @@ const PokemonList = () => {
   const [offsetPoke, setOffsetPoke] = useState(0)
   const [loading, setLoading] = useState(true)
   const [limit, setLimit] = useState(40)
+  const [listAllPokemon, setListAllPokemon] = useState<any[]>([])
+  const [inputSearch, setInputSearch] = useState('')
 
-  async function searchPokemons(offset: number) {
-    const promisesPokemonDetails: any = []
-    await api.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
-      .then((response: AxiosResponse) => {
-        response.data.results.forEach((element: any) => {
-          promisesPokemonDetails.push(api.get(element.url))
-        })
-      })
-
+  async function callPromisesToRenderPokemon(promisesPokemonDetails: any[], filter = false) {
     await Promise.all([...promisesPokemonDetails])
       .then((elements: any) => {
         let pokemonAux: PokemonDisplayProps[] = []
@@ -34,13 +28,31 @@ const PokemonList = () => {
           }]
         })
 
-        setPokemons([...pokemons, ...pokemonAux])
+        if (!filter) setPokemons([...pokemons, ...pokemonAux])
+        else {
+          setPokemons([...pokemonAux])
+          setOffsetPoke(0)
+        }
         return pokemonAux
       })
   }
 
+  async function searchPokemons(offset: number, filter = false) {
+    const promisesPokemonDetails: any = []
+    await api.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+      .then((response: AxiosResponse) => {
+        response.data.results.forEach((element: any) => {
+          promisesPokemonDetails.push(api.get(element.url))
+        })
+      })
+
+    await callPromisesToRenderPokemon(promisesPokemonDetails, filter)
+  }
+
   const handleScroll = () => {
+    console.log(`${pokemons.length} ${offsetPoke}`)
     if (pokemons.length === offsetPoke + 40 && offsetPoke <= 807) {
+      console.log('oi')
       if (pokemons.length < 767) {
         setOffsetPoke(offsetPoke + 40)
       } else {
@@ -59,19 +71,34 @@ const PokemonList = () => {
   useEffect(() => {
     async function renderFirstPokemons() {
       await searchPokemons(0)
+      await api.get('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
+        .then((response: AxiosResponse) => {
+          setListAllPokemon(response.data.results)
+        })
     }
-
     renderFirstPokemons()
   }, [])
 
   useEffect(() => {
-    const loadVideos = async () => {
+    const loadPokemon = async () => {
       setLoading(true)
       await searchPokemons(offsetPoke)
       setLoading(false)
     }
-    if (offsetPoke < 807) loadVideos()
+    if (offsetPoke < 807 && inputSearch.length === 0) loadPokemon()
   }, [offsetPoke])
+
+  async function searchPokemonByName(event: React.FormEvent<HTMLInputElement>) {
+    setInputSearch(event.currentTarget.value)
+    if (event.currentTarget.value.length >= 2) {
+      const filterPokemon = listAllPokemon
+        .filter((element: any) => element.name.includes(event.currentTarget.value))
+        .map((element: any) => api.get(element.url))
+      await callPromisesToRenderPokemon(filterPokemon, true)
+    } else if (event.currentTarget.value.length === 0) {
+      await searchPokemons(0, true)
+    }
+  }
 
   return (
     <div id="page-pokemon-list">
@@ -80,6 +107,8 @@ const PokemonList = () => {
           id="input-find-pokemon"
           type="text"
           placeholder="Pesquise"
+          value={inputSearch}
+          onChange={(event: React.FormEvent<HTMLInputElement>) => searchPokemonByName(event)}
         />
         <Link to="/" id="link-favorite-pokemon" />
       </div>
