eca89ef7bf5ffa49abbfccce62d89808dab6541e
Douglas Marcelino <douglas_champion@hotmail.com>
2020-09-11 17:11:08 -0300

    Add favorite system in PokemonDisplay component

--- src/components/PokemonDisplay/index.tsx
+++ src/components/PokemonDisplay/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import './styles.css'
@@ -8,33 +8,81 @@ export interface PokemonDisplayProps {
   number: number
   image: string
   poketypes: string
-  isFavorite: boolean
 }
 
 const PokemonDisplay: React.FC<PokemonDisplayProps> = ({
-  name, number, image, poketypes, isFavorite,
-}) => (
-  <div className={isFavorite ? 'pokemon-area' : 'pokemon-area pokemon-favorited'}>
-    <p className="pokemon-name">
-      {`#${number} ${name}`}
-    </p>
-    <div className="pokemon-image-area">
-      <img
-        className="pokemon-image"
-        src={image}
-        alt={name}
-      />
-    </div>
-    <div className="pokemon-type">{poketypes}</div>
-  </div>
-)
+  name, number, image, poketypes,
+}) => {
+  const [isFavorite, setIsFavorite] = useState(false)
 
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem('favorites') || '{}')
+    favorites.forEach((favorite: number) => {
+      if (favorite === number) setIsFavorite(true)
+    })
+  }, [])
+
+  function isFavoritesLimitReached() {
+    const favorites = JSON.parse(localStorage.getItem('favorites') || '{}')
+    return favorites.length >= 20
+  }
+
+  function updateFavoriteList() {
+    let favorites = Object.values(JSON.parse(localStorage.getItem('favorites') || '{}'))
+    let isAlreadyFavorited = false
+    let isAddFavoriteList = true
+
+    if (favorites.length > 0) {
+      favorites.forEach((favorite) => {
+        if (favorite === number) isAlreadyFavorited = true
+      })
+    }
+
+    if (!isAlreadyFavorited && !isFavoritesLimitReached()) {
+      favorites.push(number)
+      isAddFavoriteList = true
+    } else {
+      favorites = favorites.filter((element) => element !== number)
+      isAddFavoriteList = false
+    }
+
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+    return isAddFavoriteList
+  }
+
+  function updateColorPokemonArea(favorite: boolean) {
+    setIsFavorite(favorite)
+  }
+
+  function handleFavoritePokemon() {
+    const favorite = updateFavoriteList()
+    updateColorPokemonArea(favorite)
+  }
+
+  return (
+    <div
+      className={isFavorite ? 'pokemon-area pokemon-favorited' : 'pokemon-area'}
+      onClick={handleFavoritePokemon}
+    >
+      <p className="pokemon-name">
+        {`#${number} ${name}`}
+      </p>
+      <div className="pokemon-image-area">
+        <img
+          className="pokemon-image"
+          src={image}
+          alt={name}
+        />
+      </div>
+      <div className="pokemon-type">{poketypes}</div>
+    </div>
+  )
+}
 PokemonDisplay.propTypes = {
   name: PropTypes.string.isRequired,
   number: PropTypes.number.isRequired,
   image: PropTypes.string.isRequired,
   poketypes: PropTypes.string.isRequired,
-  isFavorite: PropTypes.bool.isRequired,
 }
 
 export default PokemonDisplay
