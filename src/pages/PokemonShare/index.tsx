6fd78674f94286540c91313dd0a7b201bc9d8839
Douglas Marcelino <douglas_champion@hotmail.com>
2020-09-13 23:30:31 -0300

    Update PokemonShare page

--- src/pages/PokemonShare/index.tsx
+++ src/pages/PokemonShare/index.tsx
@@ -1,17 +1,27 @@
-import React, { ReactElement, useEffect } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 
 import './styles.css'
 
 const PokemonShare = (): ReactElement => {
   const params = new URLSearchParams(window.location.search)
+  const [userName, setUserName] = useState('')
+  const [pokemonNumbers, setPokemonNumbers] = useState<string[]>([])
   useEffect(() => {
     // http://localhost:3000/pokemon-share?name=Douglas%20Thee&p=1&p=2&p=3
-    const numberFavPokemon = params.toString().split('&p=')
-    console.log(numberFavPokemon)
+    const paramsURL = params.toString().split('&p=')
+    const name = paramsURL[0].replace('name=', '')
+    const pokemon = paramsURL.slice(1)
+    setUserName(name)
+    setPokemonNumbers(pokemon)
   }, [])
   return (
     <>
-      oi
+      <ul>
+        <li>{userName}</li>
+        {pokemonNumbers.map((element: string) => (
+          <li key={element}>{element}</li>
+        ))}
+      </ul>
     </>
   )
 }
