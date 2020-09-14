0f2ab4b8183506148c70d365e9c9088f1368a6ac
Douglas Marcelino <douglas_champion@hotmail.com>
2020-09-14 20:56:53 -0300

    Add visibility of fav pokemon in PokemonShare page

--- src/pages/PokemonShare/index.tsx
+++ src/pages/PokemonShare/index.tsx
@@ -1,70 +1,97 @@
 import React, { ReactElement, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import api from '../../services/api'
 
 import './styles.css'
 
 import pokeballImg from '../../assets/images/pokeball.png'
+import PokemonShareDisplay, { PokemonShareDisplayProps } from '../../components/PokemonShareDisplay'
 
 const PokemonShare = (): ReactElement => {
   const params = new URLSearchParams(window.location.search)
   const [userName, setUserName] = useState('')
-  const [pokemonNumbers, setPokemonNumbers] = useState<string[]>([])
-  useEffect(() => {
-    // http://localhost:3000/pokemon-share?name=Douglas%20Thee&p=1&p=2&p=3
+  const [pokemons, setPokemons] = useState<PokemonShareDisplayProps[]>([])
+
+  async function callPromisesToRenderPokemon(promisesPokemonDetails: any[]) {
+    await Promise.all([...promisesPokemonDetails])
+      .then((elements: any) => {
+        let pokemonAux: PokemonShareDisplayProps[] = []
+        elements.forEach((element: any) => {
+          console.log(element)
+          pokemonAux = [...pokemonAux, {
+            name: element.data.name,
+            number: element.data.id,
+            imagePixel: element.data.sprites.front_default,
+            imageArt: element.data.sprites.other['official-artwork'].front_default || '',
+            poketypes: element.data.types[0].type.name,
+          }]
+        })
+        setPokemons([...pokemonAux])
+        return pokemonAux
+      })
+  }
+
+  async function searchFavoritesPokemon(pokemonNums: string[]) {
+    const promisesPokemonDetails: any = []
+    console.log(pokemonNums)
+    if (pokemonNums.length > 0) {
+      pokemonNums.forEach((numberPokemon: string) => {
+        promisesPokemonDetails.push(api.get(`https://pokeapi.co/api/v2/pokemon/${numberPokemon}/`))
+      })
+    }
+    await callPromisesToRenderPokemon(promisesPokemonDetails)
+  }
+
+  async function getInitialData() {
     const paramsURL = params.toString().split('&p=')
     const name = paramsURL[0].replace('name=', '').replace('+', ' ')
     const pokemon = paramsURL.slice(1)
     setUserName(name)
-    setPokemonNumbers(pokemon)
+    return pokemon
+  }
+
+  useEffect(() => {
+    async function renderFavoritesPokemon() {
+      const pokemonNums = await getInitialData()
+      console.log(pokemonNums)
+      await searchFavoritesPokemon(pokemonNums)
+    }
+
+    renderFavoritesPokemon()
   }, [])
   return (
-    <>
-      {/* <ul>
-        <li>{userName}</li>
-        {pokemonNumbers.map((element: string) => (
-          <li key={element}>{element}</li>
-        ))}
-      </ul> */}
-      <div id="page-pokemon-share">
-        <div className="user-card-top">
-          <div className="user-area-name">
-            <img src={pokeballImg} alt="Pokeball" />
-            <p>{userName}</p>
-          </div>
-          <Link to="/pokemon-list" id="link-list-pokemon">
-            Criar minha lista
-          </Link>
+
+    <div id="page-pokemon-share">
+      <div className="user-card-top">
+        <div className="user-area-name">
+          <img src={pokeballImg} alt="Pokeball" />
+          <p>{userName}</p>
         </div>
-        <hr />
-        <div className="user-card-body">
-          <p className="title">Pokemon Favoritos</p>
+        <Link to="/pokemon-list" id="link-list-pokemon">
+          Criar minha lista
+        </Link>
+      </div>
+      <hr />
+      <div className="user-card-body">
+        <p className="title">Pokemon Favoritos</p>
 
-          <div className="user-pokemon-favorites-area">
+        <div className="user-pokemon-favorites-area">
 
-            <div className="favorite-box">
-              <img
-                className="image-pokemon-pixels"
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-                alt="pokemon"
-              />
+          {pokemons.map((pokemon: PokemonShareDisplayProps) => (
+            <PokemonShareDisplay
+              key={pokemon.number}
+              name={pokemon.name}
+              number={pokemon.number}
+              imagePixel={pokemon.imagePixel}
+              imageArt={pokemon.imageArt}
+              poketypes={pokemon.poketypes}
+            />
+          ))}
 
-              <div className="pokemon-details">
-                <p>Pikachu</p>
-                <p>Number: 25</p>
-                <p>Type: eletric</p>
-              </div>
-
-              <img
-                className="image-pokemon-art"
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
-                alt="pokemon"
-              />
-            </div>
-
-          </div>
         </div>
       </div>
-    </>
+    </div>
+
   )
 }
 
